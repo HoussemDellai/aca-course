e9950ae40f3c4bc6f42ded2c1148ebe00d895554
Houssem Dellai <houssem.dellai@live.com>
2026-06-27 11:37:14 +0200

    work in progress

--- 82_aca_llm_on_serverless_gpu/aca_gemma4_31b_it_a100.no.storage.tf
+++ 82_aca_llm_on_serverless_gpu/aca_gemma4_31b_it_a100.no.storage.tf
@@ -1,79 +1,98 @@
-# resource "azurerm_container_app" "aca_gemma4_31b_it_a100" {
-#   container_app_environment_id = azurerm_container_app_environment.aca_environment.id
-#   name                         = "gemma-4-31b-it-a100"
-#   resource_group_name          = azurerm_resource_group.rg.name
-#   revision_mode                = "Single"
-#   workload_profile_name        = "GPU-NC24-A100" # "GPU-NC8as-T4"
+resource "azurerm_container_app" "aca_gemma4_31b_it_a100_no_storage" {
+  container_app_environment_id = azurerm_container_app_environment.aca_environment.id
+  name                         = "gemma-4-31b-it-a100-no-storage"
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "GPU-NC24-A100" # "GPU-NC8as-T4"
 
-#   ingress {
-#     allow_insecure_connections = true
-#     client_certificate_mode    = "ignore"
-#     external_enabled           = true
-#     target_port                = 8000
-#     transport                  = "auto"
+  ingress {
+    allow_insecure_connections = true
+    client_certificate_mode    = "ignore"
+    external_enabled           = true
+    target_port                = 8000
+    transport                  = "auto"
 
-#     traffic_weight {
-#       latest_revision = true
-#       percentage      = 100
-#     }
-#   }
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
 
-#   template {
-#     min_replicas                     = 0
-#     max_replicas                     = 1
-#     polling_interval_in_seconds      = 30
-#     cooldown_period_in_seconds       = 300
-#     termination_grace_period_seconds = 30
-#     revision_suffix                  = ""
+  template {
+    min_replicas                     = 0
+    max_replicas                     = 1
+    polling_interval_in_seconds      = 30
+    cooldown_period_in_seconds       = 1800 # 300 # 3600 # 
+    termination_grace_period_seconds = 30
+    revision_suffix                  = ""
 
-#     container {
-#       image  = "vllm/vllm-openai:gemma4-cu130"
-#       name   = "gemma4-cu130"
-#       cpu    = 1     # 24      # 8
-#       memory = "8Gi" # "220Gi" # "56Gi"
+    container {
+      image  = "vllm/vllm-openai:gemma4-cu130"
+      name   = "gemma4-31b-it"
+      cpu    = 1     # 24      # 8
+      memory = "16Gi" # "220Gi" # "56Gi"
 
-#       # The image entrypoint stays as-is; these are the args you passed after the image name.
-#       args = [
-#         "--model", "google/gemma-4-31B-it",
-#         "--tensor-parallel-size", "1",
-#         "--max-model-len", "8736",
-#         "--gpu-memory-utilization", "0.85",
-#         "--limit-mm-per-prompt", jsonencode({ "images" : 4, "videos" : 1, "audios" : 1 }),
-#         "--enable-auto-tool-choice",
-#         "--tool-call-parser", "gemma4",
-#         "--reasoning-parser", "gemma4",
-#         "--chat-template", "examples/tool_chat_template_gemma4.jinja",
-#         "--host", "0.0.0.0",
-#         "--port", "8000"
-#       ]
+      # src: https://docs.vllm.ai/projects/recipes/en/latest/Google/Gemma4.html#pip-nvidia-cuda
+      args = [
+        "--model", "google/gemma-4-31B-it",
+        "--tensor-parallel-size", "1",
+        "--max-model-len", "32768",
+        "--gpu-memory-utilization", "0.85",
+        "--limit-mm-per-prompt", jsonencode({ "images" : 4, "videos" : 1, "audios" : 1 }),
+        "--enable-auto-tool-choice",
+        "--tool-call-parser", "gemma4",
+        "--reasoning-parser", "gemma4",
+        "--chat-template", "examples/tool_chat_template_gemma4.jinja",
+        "--host", "0.0.0.0",
+        "--port", "8000"
+      ]
 
-# #       # Optional: HF token if needed for gated models
-# #       env {
-# #         name  = "HF_TOKEN"
-# #         value = ""
-# #       }
+      startup_probe {
+        path                    = "/health"
+        transport               = "HTTP"
+        port                    = "8000"
+        failure_count_threshold = "4"
+        initial_delay           = "60"
+        interval_seconds        = "240"
+        timeout                 = "240"
+      }
 
-#       # volume_mounts {
-#       #   name = "storage-comfyui"
-#       #   path = "/root/ComfyUI/"
-#       # }
-#     }
+      liveness_probe {
+        path                    = "/health"
+        transport               = "HTTP"
+        port                    = "8000"
+        failure_count_threshold = "4"
+        initial_delay           = "60"
+        interval_seconds        = "240"
+        timeout                 = "240"
+      }
 
-#     # volume {
-#     #   name         = "storage-comfyui"
-#     #   storage_name = azurerm_container_app_environment_storage.storage_aca_comfyui_nfs.name # azurerm_container_app_environment_storage.storage_aca_comfyui.name
-#     #   storage_type = "NfsAzureFile" # "AzureFile" # AzureFile (SMB) or NfsAzureFile (NFS) # Volume with Nfs Azure File storage is only supported for container app on managed environment with custom VNet.
-#     # }
+      readiness_probe {
+        path                    = "/health"
+        transport               = "HTTP"
+        port                    = "8000"
+        failure_count_threshold = "4"
+        initial_delay           = "60"
+        interval_seconds        = "240"
+        timeout                 = "240"
+      }
 
-#     http_scale_rule {
-#       name                = "http-scale"
-#       concurrent_requests = 2
-#     }
-#   }
+      # # Optional: HF token if needed for gated models
+      # env {
+      #   name  = "HF_TOKEN"
+      #   value = ""
+      # }
+    }
 
-#   depends_on = [terraform_data.add_serverless_gpu_profile_GPU-NC24-A100]
-# }
+    http_scale_rule {
+      name                = "http-scale"
+      concurrent_requests = 2
+    }
+  }
 
-# output "aca_gemma4_31b_it_a100_fqdn" {
-#   value = azurerm_container_app.aca_gemma4_31b_it_a100.ingress.0.fqdn
-# }
+  depends_on = [terraform_data.add_serverless_gpu_profile_GPU-NC24-A100]
+}
+
+output "aca_gemma4_31b_it_a100_no_storage_fqdn" {
+  value = azurerm_container_app.aca_gemma4_31b_it_a100_no_storage.ingress.0.fqdn
+}
