c33aa7ee2ed29f58cc55b126956bf4e9e5b16eac
Houssem Dellai <houssem.dellai@live.com>
2026-07-16 09:46:46 +0200

    work in progress

--- 82_aca_llm_on_serverless_gpu/aca_gemma4_31b_it_a100.tf
+++ 82_aca_llm_on_serverless_gpu/aca_gemma4_31b_it_a100.tf
@@ -29,8 +29,8 @@ resource "azurerm_container_app" "aca_gemma4_31b_it_a100" {
     container {
       image  = "vllm/vllm-openai:gemma4-cu130"
       name   = "gemma4-31b-it"
-      cpu    = 4      # 24      # 8
-      memory = "16Gi" # "16Gi" # "220Gi" # "56Gi"
+      cpu    = 8 # 4      # 24      # 8
+      memory = "24Gi" # "16Gi" # "16Gi" # "220Gi" # "56Gi"
 
       # src: https://docs.vllm.ai/projects/recipes/en/latest/Google/Gemma4.html#pip-nvidia-cuda
       args = [
